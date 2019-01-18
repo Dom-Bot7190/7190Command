0a93ab56b2cc988c4d8a96c5072c9ceee03fd9b6
John Ivison <john@ivison.org>
2019-01-17 20:01:55 -0800

    brought back deleted code

---
+++ src/frc/team7190/commands/ArcadeDrive_Command.java
@@ -0,0 +1,39 @@
+package frc.team7190.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.team7190.Robot;
+
+public class ArcadeDrive_Command extends Command {
+
+    public ArcadeDrive_Command(){
+
+        requires(Robot.DriveTrain);
+    }
+
+    @Override
+    protected void initialize(){
+
+    }
+
+    @Override
+    protected void execute(){
+
+        Robot.DriveTrain.TeleopDrive(Robot.oi.getDriverJoystick());
+    }
+
+    @Override
+    protected boolean isFinished(){
+
+        return false;
+    }
+
+    @Override
+    protected void end(){
+
+    }
+
+    @Override
+    protected void interrupted(){
+
+    }
+}
